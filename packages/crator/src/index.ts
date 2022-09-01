2d429d658fdad2a2bbfb7b278c61feba4f38aef3
protagonisths <13837050616@163.com>
2022-09-01 14:59:53 +0800

    feat(crator): generate code

--- packages/crator/src/index.ts
+++ packages/crator/src/index.ts
@@ -1,21 +1,52 @@
 import schema from './schema.json'
 import { ISchema } from './type'
 
-const recursiveSchema = (schema: ISchema) => {
+const genIndent = (len: number, size: number) => {
+  let spaces = ''
+  const all = len * size
+  for (let i = 0; i < all; i++) {
+    spaces += ' '
+  }
+  return spaces
+}
+
+const recursiveSchema = (schema: ISchema, deep: number) => {
   const { children, tagName } = schema
   let nestContent = ''
   if (children) {
+    deep += 1
     children.forEach(item => {
-      nestContent += recursiveSchema(item)
+      nestContent += recursiveSchema(item, deep)
     })
   }
-  return `<${tagName}>${nestContent}</${tagName}>`
+  return `\r\n${genIndent(deep, 2)}<${tagName}>${nestContent}</${tagName}>\r\n`
+}
+
+const genTemplate = (node: string) => {
+  return `<template>
+    ${ node }
+  </template>`
+}
+
+const genScript = () => {
+  return `<script lang="ts">
+  </script>`
+}
+
+const genStyle = () => {
+  return `<style lang="scss">
+  </style>`
 }
 
 
 const main = () => {
-  const ret = recursiveSchema(schema)
-  console.log(ret)
+  const ret = recursiveSchema(schema, 1)
+  const pageTpl = `
+  ${genScript()}
+  ${genTemplate(ret)}
+  ${genStyle()}
+  `
+  console.log(pageTpl)
 }
 
 main()
